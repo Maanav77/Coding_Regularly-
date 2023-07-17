557c51566dca6ce691100fe6bf61886e42101a8b
Maanav77 <93425189+Maanav77@users.noreply.github.com>
2023-07-17 09:22:00 +0530

    Day 11

---
+++ Day 11 - Sorting a java.util.Map by its values.java
@@ -0,0 +1,31 @@
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Day011 {
+
+    public static void main(String[] args) {
+        Map<String, Integer> unsortedMap = Map.of(
+                "three", 3,
+                "one", 1,
+                "four", 4,
+                "five", 5,
+                "two", 2
+        );
+
+        Map<String, Integer> sortedMap = sortByValue(unsortedMap);
+
+        System.out.println(sortedMap);
+    }
+
+    private static Map<String, Integer> sortByValue(Map<String, Integer> unsortedMap) {
+        Map<String, Integer> sortedMap = new LinkedHashMap<>();
+
+        unsortedMap.entrySet().stream()
+                   .sorted(Map.Entry.comparingByValue())
+                   .forEach(entry -> sortedMap.put(entry.getKey(), entry.getValue()));
+
+        return Collections.unmodifiableMap(sortedMap);
+    }
+}
