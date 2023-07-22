0394f7c3aaf27c2ed23acade8e4600ca3ef30b76
Maanav77 <93425189+Maanav77@users.noreply.github.com>
2023-07-22 09:22:00 +0530

    Day 16

---
+++ Day 16 - Joining Strings.java
@@ -0,0 +1,13 @@
+public class Day016 {
+
+    public static void main(String[] args) {
+        System.out.println(createSql("id", "name", "coutry", "gender"));
+    }
+
+    private static String createSql(String... columns) {
+        return new StringBuilder("SELECT ")
+                .append(String.join(", ", columns))
+                .append(" FROM PEOPLE")
+                .toString();
+    }
+}
