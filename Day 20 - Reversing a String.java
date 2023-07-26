c185df51a3af80f29ce1d84dac525b3014291e1a
Maanav77 <93425189+Maanav77@users.noreply.github.com>
2023-07-26 10:23:00 +0530

    Day 20

---
+++ Day 20 - Reversing a String.java
@@ -0,0 +1,8 @@
+public class Day020 {
+
+    public static void main(String[] args) {
+        var original = "moc.ipataergeht";
+        var reversed = new StringBuilder(original).reverse().toString();
+        System.out.println(reversed);
+    }
+}
